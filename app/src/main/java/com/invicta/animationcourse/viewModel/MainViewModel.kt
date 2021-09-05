5db6dc899ad1cce9b33e9937071a5fe4b785475a
ErfanDP <delavari.erfan@yahoo.com>
2021-09-05 10:23:19 +0430

    FIX: configuration change crash on PlaceHolder Animation

--- app/src/main/java/com/invicta/animationcourse/viewModel/MainViewModel.kt
+++ app/src/main/java/com/invicta/animationcourse/viewModel/MainViewModel.kt
@@ -3,9 +3,10 @@ package com.invicta.animationcourse.viewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.invicta.animationcourse.data.ButtonType
+import com.invicta.animationcourse.utils.SingleLiveData
 
 class MainViewModel : ViewModel() {
-	val selectedItem = MutableLiveData<ButtonType>()
+	val selectedItem = SingleLiveData<ButtonType>()
 	
 	fun selectItem(buttonType: ButtonType) {
 		selectedItem.value = buttonType
