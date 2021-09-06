eec5a4f316de8b2a6eb69bea4fac18767e6d81c1
ErfanDP <delavari.erfan@yahoo.com>
2021-09-06 15:16:04 +0430

    doing multiple animations on a button using AnimationSets

--- app/src/main/java/com/invicta/animationcourse/view/fragments/ObjectAnimatorFragment.kt
+++ app/src/main/java/com/invicta/animationcourse/view/fragments/ObjectAnimatorFragment.kt
@@ -1,7 +1,10 @@
 package com.invicta.animationcourse.view.fragments
 
+import android.animation.AnimatorSet
+import android.animation.ArgbEvaluator
 import android.animation.ObjectAnimator
 import android.animation.ValueAnimator
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -15,6 +18,11 @@ import com.invicta.animationcourse.databinding.FragmentObjectAnimatorBinding
 class ObjectAnimatorFragment: Fragment() {
 
 	private lateinit var binding :FragmentObjectAnimatorBinding
+	private lateinit var scaleX : ObjectAnimator
+	private lateinit var translationY : ObjectAnimator
+	private lateinit var colorAnim : ObjectAnimator
+	
+	
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		
@@ -28,14 +36,43 @@ class ObjectAnimatorFragment: Fragment() {
 										  R.layout.fragment_object_animator,
 										  container,
 										  false)
-		binding.buttonClickMe.setOnClickListener { button->
-			val objectAnimator = ObjectAnimator.ofFloat(button,"scaleX",5F)
-			objectAnimator.duration = 1000
-			objectAnimator.repeatMode = ValueAnimator.REVERSE
-			objectAnimator.repeatCount = ValueAnimator.INFINITE
-			objectAnimator.start()
+		initObjectAnimators()
+		val animatorSet = AnimatorSet()
+		animatorSet.duration = 1000
+		animatorSet.playTogether(scaleX,translationY,colorAnim)
+		binding.buttonClickMe.setOnClickListener {
+			if(animatorSet.isRunning){
+				animatorSet.end()
+			}else{
+				animatorSet.start()
+			}
 		}
 		return binding.root
+	}
+	
+	private fun initObjectAnimators() {
+		scaleX = ObjectAnimator.ofFloat(binding.buttonClickMe,
+										"scaleX",
+										3F)
+		initObjectAnimatorFields(scaleX)
+		
+		translationY = ObjectAnimator.ofFloat(binding.buttonClickMe,
+											  "translationY",
+											  300F)
+		initObjectAnimatorFields(translationY)
+		
+		colorAnim = ObjectAnimator.ofObject(binding.buttonClickMe,
+											"textColor",
+											ArgbEvaluator(),
+											Color.RED,
+											Color.GREEN)
+		initObjectAnimatorFields(colorAnim)
+		
+	}
+	
+	private fun initObjectAnimatorFields(objectAnimator: ObjectAnimator) {
+		objectAnimator.repeatMode = ValueAnimator.REVERSE
+		objectAnimator.repeatCount = ValueAnimator.INFINITE
 	}
 	
 	companion object {
